8de21c0e4a5be255bd88538a94f0a6762be22b0d
Billy-Mapessa <omwagajohn@gmail.com>
2023-10-09 19:48:28 -0700

    if_else_while

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - Entry point. Prints numbers 0-9 and lowercase hexadecimal characters a-f.
+ * main - Prints numbers between 0 to 9 and letters between a to f.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char letter;
-	for (letter = '0'; letter <= '9'; letter++)
-	{
-		putchar(letter);
-	}
-	for (letter = 'a'; letter <= 'f'; letter++)
-	{
-		putchar(letter);
-	}
+	int i;
 
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
 	putchar('\n');
 	return (0);
 }
+
 
