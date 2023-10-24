90e8ab987db0819d807cb89888dcf8fee463f39d
Billy-Mapessa <omwagajohn@gmail.com>
2023-10-24 14:55:00 -0700

    even more pointers

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  * _strstr - main function
+  *
+  * @needle: first parameter
+  *
+  * @haystack: second parameter
+  *
+  * Return: Always 0.
+  */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+	char *x = haystack;
+	char *y = needle;
+
+	while (*x == *y && *y != '\0')
+	{
+	x++;
+	y++;
+	}
+	if (*y == '\0')
+	return (haystack);
+	}
+	return (0);
+}
+
