60173c0a969fa0b55fcdaafd3a016dc737069ceb
Billy-Mapessa <omwagajohn@gmail.com>
2023-10-24 02:03:17 -0700

    more pointers

---
+++ 0-memset.c
@@ -0,0 +1,23 @@
+
+#include "main.h"
+
+/**
+ *_memset - main function of the prototype
+ *
+ *@x: The value used to fill the memory
+ *
+ *@y: Function paramater
+ *
+ *@z: Function parameter
+ *
+ *Return: y
+ */
+char *_memset(char *y, char x, unsigned int z)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	y[a] = x;
+	return (y);
+}
+
